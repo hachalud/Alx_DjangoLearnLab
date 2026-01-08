a87fc1455091173b3952d7d796d68c13241e765a
Hachalu.Diriba <hachaludi@awash.local>
2026-01-08 08:41:09 +0300

    last admin interfece

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    published_year = models.IntegerField()
+    publication_year = models.IntegerField()
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.published_year})"
+        return f"{self.title} by {self.author} ({self.publication_year})"
