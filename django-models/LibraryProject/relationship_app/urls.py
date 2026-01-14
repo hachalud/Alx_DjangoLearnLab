ee7646599360720530ad64dd8d59466f2c6bbdd4
hachalud <hachaludiriba69@gmail.com>
2026-01-14 15:38:14 +0300

    view

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,7 @@
+#  include URL patterns that route to the newly created views. Make sure to link both the function-based and class-based views.
+from django.urls import path
+from .views import list_books, LibraryDetailView
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Create a function-based view in relationship_app/views.py that lists all books stored in the database.
+# This view should render a simple text list of book titles and their authors.
+from django.http import HttpResponse
+from .models import Book
+def list_books(request):
+    books = Book.objects.all()
+    response_content = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(response_content, content_type="text/plain")
+
+# Create a class-based view in relationship_app/views.py that displays details for a specific library, listing all books available in that library.
+# Utilize Django’s ListView or DetailView to structure this class-based view.
+from django.views.generic import DetailView
+from .models import Library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        return context
