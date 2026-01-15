25c3883191cdc7dcee06304c3e2c38544129d212
hachalud <hachaludiriba69@gmail.com>
2026-01-15 16:13:01 +0300

    role

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -34,3 +34,34 @@ def register(request):
         form = UserCreationForm()
     
     return render(request, 'relationship_app/register.html', {'form': form})
+from django.contrib.auth import views as auth_views
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+def is_admin(user):
+    return user.is_authenticated and user.profile.role == 'admin'
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html', {'user': request.user})
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+def is_librarian(user):
+    return user.is_authenticated and user.profile.role == 'Librarian'
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html', {'user': request.user})
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+def is_member(user):
+    return user.is_authenticated and user.profile.role == 'Member'
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html', {'user': request.user})
