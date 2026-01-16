ef943dbec4d2fa39e61d80adcb64bc8137c393a4
hachalud <hachaludiriba69@gmail.com>
2026-01-16 06:57:27 +0300

    custom permission test agai once

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
-    path('books/add/', add_book, name='add_book'),
-    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', delete_book, name='delete_book'),
+    path('books/add_book/', add_book, name='add_book'),
+    path('books/edit_book/<int:pk>/', edit_book, name='edit_book'),
+    path('books/delete_book/<int:pk>/', delete_book, name='delete_book'),
 ]
