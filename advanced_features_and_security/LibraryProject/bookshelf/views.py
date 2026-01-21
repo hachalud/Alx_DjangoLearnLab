f2e833bf23e619b7d2483fbb590c4a6715de1d4f
hachalud <hachaludiriba69@gmail.com>
2026-01-21 23:06:59 +0300

    security check 2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -98,3 +98,28 @@ def book_list(request):
     response["Content-Security-Policy"] = "default-src 'self';"
 
     return response
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import ExampleForm   # ✅ REQUIRED IMPORT
+
+
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "bookshelf/book_list.html", {"books": books})
+
+
+@permission_required("bookshelf.can_view", raise_exception=True)
+def books(request):
+    books = Book.objects.all()
+    return render(request, "bookshelf/books.html", {"books": books})
+
+
+def example_form_view(request):
+    """
+    View demonstrating secure form handling using ExampleForm.
+    """
+    form = ExampleForm()
+    return render(request, "bookshelf/form_example.html", {"form": form})
