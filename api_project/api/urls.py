128f9cdba2b8403111a555109b170c08f04b6891
Hachalu Diriba <hachaludiriba69@gmail.com>
2026-01-30 15:15:17 +0300

    api configure crude

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import BookList
-
+from rest_framework import routers
+from api import views
+from .views import BookList, BookViewSet
+from django.urls import include
+from rest_framework import defaultsRouter
+router = defaultsRouter()
+router.register(r'books', views.BookViewSet, basename='book_all')
+router = routers.DefaultRouter()
+router.register(r'books', views.BookViewSet)
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('api/', include('api.urls')),
+    path('', include(router.urls)),
 ]
