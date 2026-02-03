76de6b2b982d8ca7cfd252ab587a699808b48e5a
hachalud <hachaludiriba69@gmail.com>
2026-02-03 10:04:31 +0300

    out boxs app

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,17 +1,48 @@
 from django.db import models
 
-# Create your models here.
-class Author (models.Model):
+# ------------------------------
+# Author model
+# ------------------------------
+class Author(models.Model):
+    """
+    Author Model:
+    -------------
+    Represents an author who can write multiple books.
+
+    Fields:
+    - name: stores the author's name.
+    Relationship:
+    - One Author can have many Books (one-to-many).
+    """
     name = models.CharField(max_length=100)
-    bio = models.TextField()
 
     def __str__(self):
         return self.name
 
+
+# ------------------------------
+# Book model
+# ------------------------------
 class Book(models.Model):
+    """
+    Book Model:
+    -----------
+    Represents a book written by an author.
+
+    Fields:
+    - title: title of the book
+    - publication_year: the year the book was published (integer)
+    - author: foreign key linking to Author
+    Relationship:
+    - Each book belongs to one Author
+    """
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
-    published_year = models.DateField()
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(
+        Author,
+        related_name='books',
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.title
